7bd8c2111e39c07471716f7a0561ca211487a6a8
rzubr <67950785+rzubrr@users.noreply.github.com>
2021-12-17 09:23:02 +0100

    The original script

--- script.js
+++ script.js
@@ -11,37 +11,6 @@ function losuj() {
     return Math.floor(Math.random() * (20 - (-10)) + (-10))
 }
 
-function guess () {
-    x = losuj()
-    y = losuj()
-    console.log(x)
-    console.log(y)
-    op = Math.floor(Math.random() * 3)
-    console.log("Działanie: " + op)
-    if (op == 0) {
-        random[count] = x + y
-        h1.innerText = `${x} + ${y}`
-    }
-    else if (op == 1) {
-        random[count] = x - y
-        h1.innerText = `${x} - ${y}`
-    }
-    else {
-        random[count] = x * y
-        h1.innerText = `${x} * ${y}`
-    }
-    console.log("Wynik = " + random[count])
-    console.log(random)
-    guesses[count] = input.value;
-    console.log("Zgadnięcie: " + guesses[count])
-    if (guesses[count] == random[count]) {
-        console.log("ZGADŁ")
-        guessed++
-    }
-    count++
-}
-
-
 
 div = document.createElement("div")
 div.classList.add("main")
@@ -51,27 +20,46 @@ button = document.createElement("button")
 button.innerText = "START"
 input = document.createElement("input")
 input.type = "number"
-button_submit = document.createElement("button")
-button_submit.innerText = "NEXT"
 
 cont.appendChild(div)
 div.appendChild(h1)
 div.appendChild(button)
 
 button.addEventListener('click', () => {
-        div.removeChild(button)
-        div.appendChild(input)
-        div.appendChild(button_submit)
-        guess()
-})
-
-button_submit.addEventListener('click', () => {
     if (count < 5) {
-        guess()
-
+        div.removeChild(h1)
+        div.removeChild(button)
+        x = losuj()
+        y = losuj()
+        console.log(x)
+        console.log(y)
+        op = Math.floor(Math.random() * 3)
+        console.log("Działanie: " + op)
+        if (op == 0) {
+            random[count] = x + y
+            h1.innerText = `${x} + ${y}`
+        }
+        else if (op == 1) {
+            random[count] = x - y
+            h1.innerText = `${x} - ${y}`
+        }
+        else {
+            random[count] = x * y
+            h1.innerText = `${x} * ${y}`
+        }
+        button.innerText = 'NEXT'
+        console.log("Wynik = " + random[count])
+        console.log(random)
+        guesses[count] = input.value;
+        console.log("Zgadnięcie: " + guesses[count])
+        if (guesses[count] == random[count-1]) {
+            console.log("ZGADŁ")
+            guessed++
+        }
+        count++
         div.appendChild(h1)
         div.appendChild(input)
-        div.appendChild(button_submit)
+        div.appendChild(button)
         console.log(guesses)
         console.log(random)
     }
@@ -85,7 +73,11 @@ button_submit.addEventListener('click', () => {
         guesses.splice(0, 1)
         console.log(guesses)
         console.log(random)
+
+        div.removeChild(button)
         div.removeChild(input)
+
         h1.innerText=`You've guessed ${guessed} out of 5`
+        
     }
 })
